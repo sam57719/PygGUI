cbb3c7d89be6263ec91ce8ece8d4ca565f6df2c6
Sam Cork <sam57719@gmail.com>
2022-05-02 23:53:35 +0100

    Add setup.py and requirements.txt to allow installing

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PygGUI',
+    version='0.1',
+    packages=['pyggui', 'pyggui.core', 'pyggui.widgets'],
+    url='',
+    license='',
+    author='Sam Cork',
+    author_email='',
+    description='Pygame GUI'
+)
