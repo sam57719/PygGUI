cf780ecf5e4b5bf5df04a76af8323e128456be04
Sam Cork <sam57719@gmail.com>
2022-05-13 21:25:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='PygGUI',
     version='0.1',
     packages=['pyggui', 'pyggui.core', 'pyggui.widgets'],
-    url='',
+    url='https://github.com/sam57719/PygGUI',
     license='',
     author='Sam Cork',
-    author_email='',
+    author_email='sam57719+pyggui@gmail.com',
     description='Pygame GUI'
 )
